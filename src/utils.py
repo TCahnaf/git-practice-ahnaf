bfee23b7cdcfd7e1ad16dc41eb0c45d591e4dc00
TCahnaf <ahnafchwd164@gmail.com>
2026-08-01 22:34:59 -0400

    added basic error handling

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,16 @@
 def add(a,b):
+    if type(a) != int or type(b) != int:
+        return 'Sorry but both inputs must be integers'
     return a+b
 
 def subtract(a,b):
+    if type(a) != int or type(b) != int:
+        return 'Sorry but both inputs must be integers'
     return a-b
 
 def multiply(a,b):
+    if type(a) != int or type(b) != int:
+        return 'Sorry but both inputs must be integers'
     return a*b
+
+
